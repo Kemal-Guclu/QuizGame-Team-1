3451adca5d26ab9bef7df354d7b5f8f1afa3cbf2
chimpy7 <aleksandar199@hotmail.com>
2024-11-07 13:48:09 +0100

    javascript file med for each för att loopa igenom allt

---
+++ func.js
@@ -0,0 +1,10 @@
+//fetch()
+
+const answwers = document.querySelectorAll('.answercontainer')
+
+answwers.forEach( function(itemss){
+itemss.addEventListener('click',function(){
+ alert("heyski")
+})
+    
+})
