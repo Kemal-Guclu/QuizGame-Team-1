4bda07ff29a70e0df447adfe063acbded17504a1
Niklas-Nordin <niklas.nordin@chasacademy.se>
2024-11-14 15:42:19 +0100

    Lägger till en nedräknare, som även döljer frågor & svarsalternativ, om tiden går ut. Visar annan output istället då.

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
+const questionAnswer = document.getElementById("questionAnswer");
+const mainContainer = document.getElementById("maincontainer");
+const toSlow = document.createElement("div");
+toSlow.classList.add("toSlow");
 
 
+const time = document.getElementById("timer");
+let count = 1;
+
+//Funktion som räknar ned tiden som man har per fråga.
+let timeLeft = setInterval(function() {
+
+    count--;
+    if (count === 0) {
+
+        clearInterval(timeLeft);
+        questionAnswer.style.display = "none";
+        time.innerHTML = "";
+
+        //Skapar paragrafer som berättar att tiden gick ut.
+        const timesUp = document.createElement("p");
+        timesUp.classList.add("times-up");
+        timesUp.textContent = "Times up! You took too long...";
+
+        const letsContinue = document.createElement("p");
+        letsContinue.classList.add("lets-continue");
+        letsContinue.textContent = "Press the button to continue";
 
 
+        toSlow.appendChild(timesUp);
+        toSlow.appendChild(letsContinue);
+        mainContainer.appendChild(toSlow);
+
+        const btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.textContent = "Continue";
+        toSlow.append(btn);
+
+        //Skapar en knapp för att komma vidare till nästa fråga
+
+        
+    } else {
+        time.innerHTML = count;
+    }
+    
+    
+
+}, 1000);
 
 
 
