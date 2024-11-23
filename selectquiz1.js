31812ec98032c685bc1d21f619259486d5817540
Kemal-Guclu <kemal.guclu@chasacademy.se>
2024-11-23 19:37:15 +0100

    Deleted old files

--- selectquiz1.js
+++
@@ -1,8 +0,0 @@
-addEventListener("DOMContentLoaded", (event) => {
-  renderCards();
-  card1.addEventListener("click", renderCards);
-});
-
-async function redirect(event) {
-  window.location.href = "./questions.html?quizid=1";
-}
